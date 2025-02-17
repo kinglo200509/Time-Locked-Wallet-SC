b72d6a42104c67fbb8b3feaa6c2324e928c4b7b0
saswat kumar <saaswatkumaradibhatia@gmail.com>
2025-02-17 22:27:03 +0530

    Can store eth in the contract(now safe)

--- contracts/TimeLocked.sol
+++ contracts/TimeLocked.sol
@@ -3,9 +3,33 @@ pragma solidity ^0.8.28;
 import "hardhat/console.sol";
 
 contract TimeLocked {
-    address public user; 
+    address  public owner; 
+
     constructor() {
-        user = msg.sender;
+        owner= msg.sender;
+    }
+
+    // function to storeEth
+    // simply doing this will send eth in your contract
+    function StoreEth() public payable {
+        // how much balance is there in smart contract 
+        // this is an contract instance whihc will be converted to address object
+        // address(this).balance alrrady cointain msg.value
+        // no need to manually assign eth 
+        
     }
     
 }
+
+
+
+
+
+
+
+
+
+
+
+        // .tranfer is only applicble for address payable ;
+        // owner.transfer(msg.value);
