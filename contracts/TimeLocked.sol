c629e4946d6bf531de34262258fe5e3799412477
saswat kumar <saaswatkumaradibhatia@gmail.com>
2025-02-18 15:24:40 +0530

    Testing the contract on test.js

--- contracts/TimeLocked.sol
+++ contracts/TimeLocked.sol
@@ -11,12 +11,20 @@ contract TimeLocked {
 
     // function to storeEth
     // simply doing this will send eth in your contract
-    function StoreEth() public payable {
+    function StoreEth() public payable returns(uint){
         // how much balance is there in smart contract 
-        // this is an contract instance whihc will be converted to address object
+        // this is an contract instance which will be converted to address object-(address(this).balance)
         // address(this).balance alrrady cointain msg.value
-        // no need to manually assign eth 
-        
+        // no need to manually assign eth     
+        uint  eth =address(this).balance;
+        return(eth);
+    }
+
+    // function to show eth 
+    // 
+
+    function withdraw() public payable{
+
     }
     
 }
--- test/Lock.js
+++ test/Lock.js
@@ -1,6 +1,42 @@
 const {expect} = require("chai");
 const { ethers } = require("hardhat");
 
+// ethers verion 6
+
 describe("TimeLocked" , function(){
-  
+  let Contract , contract;
+
+
+  //   In Ethers v6, deploy() internally handles:
+  // Sending the transaction to deploy the contract.
+  // Waiting for the transaction to be mined.
+  // Returning the deployed contract.
+
+  // before testing create an instance 
+  beforeEach(async()=>{
+    Contract = await ethers.getContractFactory("TimeLocked");
+    contract =await Contract.deploy();
+    //  Use waitForDeployment(): If you need fine-grained control
+    await contract.waitForDeployment();
+    console.log( await contract.getAddress());
+  })
+
+  it("This is should recive some eth in the address of the account ",async  ()=> {
+    // should pass the value as an object pareEther-
+    const tx = await contract.StoreEth({value: ethers.parseEther("1.0")});
+    // since its a payable function we have to wait
+    await tx.wait();
+    // getBalance provider is a read-only abstraction to access the blockchain data.
+    const balance = await ethers.provider.getBalance(await contract.getAddress());
+    console.log( `THis is the address of the contract ${await contract.getAddress()}`)
+    console.log(`This is the balance: ${balance}`);
+
+    expect(balance.toString()).to.equal(ethers.parseEther("1.0").toString());
+
+  })
 })
+
+
+
+// describe -> ethers.getCOntractFActory ->deploy->waitFOrDe.. ->contract.getAddress()
+// with payable functions take transactions  and wait() for the transaction for to complete
